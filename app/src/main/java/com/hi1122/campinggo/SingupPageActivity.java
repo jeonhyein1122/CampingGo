b7ca599f3dd9556c781b6d8e5633e11c69144a81
jeonhyein <hihi4835@gmail.com>
2021-02-17 16:53:01 +0900

    signuppage start

--- app/src/main/java/com/hi1122/campinggo/SingupPageActivity.java
+++ app/src/main/java/com/hi1122/campinggo/SingupPageActivity.java
@@ -1,8 +1,11 @@
 package com.hi1122.campinggo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class SingupPageActivity extends AppCompatActivity {
@@ -11,7 +14,10 @@ public class SingupPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup_page);
+
     }
+
+
 
     public void clickImage(View view) {
     }
